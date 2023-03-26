68fb61ce792299aea0c33a42fa9e0b65ef106a5c
Leonardo Llanes <llanesleonardo.developer@gmail.com>
2023-03-26 01:08:27 -0700

    add README

---
+++ main.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+<div class="container vh-100">
+<div class="row vh-100">
+  <div class="col-12 d-flex justify-content-center align-items-center">
+    <img
+      src="https://3lstrategy.nyc3.cdn.digitaloceanspaces.com/tommy%2Flogos%2Flogotipo-minified.png"
+      alt=""
+      class="logo"
+    />
+  </div>
+</div>
+</div>
+`;
