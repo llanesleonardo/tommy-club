f20413d69a6a8155bd3a0bd25a2b7039e3fc4a03
Leonardo Llanes <llanesleonardo.developer@gmail.com>
2023-03-27 01:31:08 -0700

    avance en enlaces y redes sociales

--- main.js
+++ main.js
@@ -11,11 +11,7 @@ document.querySelector("#app").innerHTML = `
   <div class="container vh-100">
     <div class="row vh-100">
       <div class="col-12 d-flex justify-content-center align-items-center">
-        <img
-          src="https://3lstrategy.nyc3.cdn.digitaloceanspaces.com/tommy%2Flogos%2Flogotipo-minified.png"
-          alt=""
-          class="logo"
-        />
+
       </div>
     </div>
   </div>
--- template/headers/Header-01.js
+++ template/headers/Header-01.js
@@ -3,8 +3,23 @@ import getHash from "../../utils/getHash";
 const Headers = () => {
   getHash();
   const view = `
-    <div class="top-header">HEADER</div>
-    <nav></nav>
+    <div class="top-header"><img class="img-logo" src="https://3lstrategy.nyc3.cdn.digitaloceanspaces.com/tommy%2Flogos%2Flogotipo-minified.png"/></div>
+    <nav class="main-nav">
+    <div class="wrapper-navs">
+      <ul class="nav-list">
+        <li class="nav-item"><a href="">Bienvenido</a></li>
+        <li class="nav-item"><a href="">¿Como comprar?</a></li>
+        <li class="nav-item"><a href="">El club</a></li>
+        <li class="nav-item"><a href="">El Equipo</a></li>
+        <li class="nav-item"><a href="">Contacto</a></li>
+      </ul>
+      <ul class="social-nav">
+        <li class="nav-social-item"><a href=""><object data="https://3lstrategy.nyc3.cdn.digitaloceanspaces.com/tommy%2Fredes%2Felementos-04.svg" width="30" height="30"></object></a></li>
+        <li class="nav-social-item"><a href=""><object data="https://3lstrategy.nyc3.cdn.digitaloceanspaces.com/tommy%2Fredes%2Felementos-06.svg" width="30" height="30"></object></a></li>
+        <li class="nav-social-item"><a href=""><object data="https://3lstrategy.nyc3.cdn.digitaloceanspaces.com/tommy%2Fredes%2Felementos-03.svg" width="30" height="30"></object></a></li>
+      </ul>
+      </div>
+    </nav>
     `;
   return view;
 };
