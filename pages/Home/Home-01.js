ac852a1cf163dec918a933ed364a6e374a260527
Leonardo Llanes <llanesleonardo.developer@gmail.com>
2023-03-27 22:52:48 -0700

    roadmap

--- pages/Home/Home-01.js
+++ pages/Home/Home-01.js
@@ -4,6 +4,7 @@ import Intro from "../../template/maincontent/introductions/Intro-01";
 import Promo from "../../template/maincontent/promo/Promo-01";
 import Especs from "../../template/maincontent/especs/Especs-01";
 import Club from "../../template/maincontent/club/Club-01";
+import Roadmap from "../../template/maincontent/roadmap/Roadmap-01";
 
 const Home = async () => {
   const view = `
@@ -13,6 +14,7 @@ const Home = async () => {
             ${Promo()}
             ${Especs()}
             ${Club()}
+            ${Roadmap()}
         </div>
     `;
 
---
+++ template/maincontent/roadmap/Roadmap-01.js
@@ -0,0 +1,26 @@
+import "./roadmap-01.css";
+
+const Roadmap = () => {
+  const view = `
+        <div class="main-roadmap">
+            <div class="main-roadmap-block">
+                <h2 class="main-roadmap-title">ROADMAP</h2>
+                <ol class="main-roadmap-list">
+                    <li class="main-roadmap-listitem"><img src="" class="main-roadmap-listitem-img"/> Proof of idea </li>
+                    <li class="main-roadmap-listitem"><img src="" class="main-roadmap-listitem-img"/> Airdropped and Sales of Tommy NFT </li>
+                    <li class="main-roadmap-listitem"><img src="" class="main-roadmap-listitem-img"/> Youtube channel, Podcast, Social networks</li>
+                    <li class="main-roadmap-listitem"><img src="" class="main-roadmap-listitem-img"/> Member exclusive store, limited edition apparel</li>
+                    <li class="main-roadmap-listitem"><img src="" class="main-roadmap-listitem-img"/> Crypto rewards / “We have to invent some game”</li>
+                    <li class="main-roadmap-listitem"><img src="" class="main-roadmap-listitem-img"/> Ayudar a los artistas a llevar su arte digital a la web 3.0</li>
+                    <li class="main-roadmap-listitem"><img src="" class="main-roadmap-listitem-img"/> Se empieza la escuela de tommy para emprendedores 3.0</li>
+                    <li class="main-roadmap-listitem"><img src="" class="main-roadmap-listitem-img"/> Se inicia la tokenización de otras ideas innovadoras (inversiones)</li>
+                    <li class="main-roadmap-listitem"><img src="" class="main-roadmap-listitem-img"/> Nuevas ideas para colaborar en el ambiente crypto</li>
+                </ol>
+            </div>
+        </div>
+    `;
+
+  return view;
+};
+
+export default Roadmap;
