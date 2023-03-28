42dbc6a3da2344ea8455524ea22fecd927b5f565
Leonardo Llanes <llanesleonardo.developer@gmail.com>
2023-03-27 22:23:12 -0700

    especs

--- pages/Home/Home-01.js
+++ pages/Home/Home-01.js
@@ -2,6 +2,7 @@ import "./home-01.css";
 import Hero from "../../template/maincontent/heros/Hero-01";
 import Intro from "../../template/maincontent/introductions/Intro-01";
 import Promo from "../../template/maincontent/promo/Promo-01";
+import Especs from "../../template/maincontent/especs/Especs-01";
 
 const Home = async () => {
   const view = `
@@ -9,6 +10,7 @@ const Home = async () => {
             ${Hero()}
             ${Intro()}
             ${Promo()}
+            ${Especs()}
         </div>
     `;
 
